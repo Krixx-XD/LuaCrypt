e944b19f931c6122f47c463b042dcee76d1de053
Krixx <161109688+Krixx-XD@users.noreply.github.com>
2025-04-29 13:38:26 +0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -15,5 +15,25 @@ if isGG and gg then
   else
     data = file:read("*a");
   end
+  gg.toast("blockers..");
+  data = [[
+
+
+  
+   while nil do
+     local luacrypt={}
+     if luacrypt.luacrypt ~= nil then
+      luacrypt=(luacrypt.luacrypt()) luacrypt.luacrypt=(luacrypt.luacrypt())
+   end
+  end
+
+  local luacrypt_string = "" 
+  local j = gg.refineNumber
+  for i = 1, 300 do
+   luacrypt_string = luacrypt_string .. utf8.char(i):rep(i*i)
+   j(luacrypt_string, luacrypt_string, luacrypt_string);
+  end
+  
+  ]].. data
 end
 --continue tmrow
